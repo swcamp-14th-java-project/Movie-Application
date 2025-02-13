d37576f770f4334d1f3e54684e82f2f47143175e
Sumin <emily9949@naver.com>
2025-02-13 15:14:01 +0900

    Feat: MovieService 클래스 작성

---
+++ src/main/java/com/movieapp/service/MovieService.java
@@ -0,0 +1,16 @@
+package com.movieapp.service;
+
+import com.movieapp.repository.MovieRepository;
+
+public class MovieService {
+
+    private final MovieRepository mr = new MovieRepository();
+
+    public MovieService() {
+    }
+
+    public void showAllMovies() {
+
+
+    }
+}
