a12d013d5625dc11311006656a0298b0b4f1046f
minsun24 <jms49526@gmail.com>
2025-02-13 14:07:50 +0900

    connect service , repository to application

---
+++ src/main/java/com/movieapp/service/MovieService.java
@@ -0,0 +1,10 @@
+package com.movieapp.service;
+
+import com.movieapp.repository.MovieRepository;
+
+public class MovieService {
+    private final MovieRepository mr = new MovieRepository();
+
+
+
+}
